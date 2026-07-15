b8fe28ac0c2da2ca541e9c860aa9cbe47c3f3727
Mithra V <mithrav2005@gmail.com>
2026-07-15 15:24:59 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtMsCjwbPBFZLG9I9D_I53UoEkiD87Deo",
-  authDomain: "acadvault-3cfae.firebaseapp.com",
-  projectId: "acadvault-3cfae",
-  storageBucket: "acadvault-3cfae.firebasestorage.app",
-  messagingSenderId: "942235321776",
-  appId: "1:942235321776:web:040751d03d223d9257c275"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
